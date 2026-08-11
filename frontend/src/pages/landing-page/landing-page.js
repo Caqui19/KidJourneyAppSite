c6476377827f7c98b717bee47cb23f129f394eca
Caqui19 <162844832+Caqui19@users.noreply.github.com>
2026-08-11 03:14:22 +0000

    Estruturação Base do Main

--- frontend/src/pages/landing-page/landing-page.js
+++ frontend/src/pages/landing-page/landing-page.js
@@ -7,3 +7,18 @@ function fecharMenu() {
     const menu = document.getElementById('menu');
     menu.style.display = 'none';
 }
+
+function visualizarSenha() {
+    const campoSenha = document.querySelectorAll('.senhaContainer input');
+    const iconesSenha = document.querySelectorAll('.vizualizarSenha');
+
+    campoSenha.forEach(field => {
+        field.type = field.type === 'password' ? 'text' : 'password';
+    });
+
+    iconesSenha.forEach(icon => {
+        icon.textContent = campoSenha[0].type === 'password'
+            ? 'visibility_off'
+            : 'visibility';
+    });
+}
