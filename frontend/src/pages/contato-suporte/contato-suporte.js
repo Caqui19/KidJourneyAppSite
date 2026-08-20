a66dc2b0e109da8267279c874470677483da3ae4
Caqui19 <caqui19iao@gmail.com>
2026-08-19 21:10:47 -0300

    Alterações Finais Parte 1

---
+++ frontend/src/pages/contato-suporte/contato-suporte.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = 'flex';
+}
+
+function fecharMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = 'none';
+}
